71b16f52d788acf12da9850580811389dd2d563a
Jason Bornstein <131717043+jasonbornsteinMOOV@users.noreply.github.com>
2023-08-01 09:39:03 -0400

    add hook to use the template to define the output file name

--- pkg/xsd2go/generate.go
+++ pkg/xsd2go/generate.go
@@ -2,12 +2,22 @@ package xsd2go
 
 import (
 	"fmt"
+	"path"
+	"strings"
 
 	"github.com/moov-io/xsd2go/pkg/template"
 	"github.com/moov-io/xsd2go/pkg/xsd"
 )
 
 func Convert(xsdPath, goModule, outputDir string, xmlnsOverrides []string, templateName string, outputFile string) error {
+	if strings.Contains(templateName, ".tgo") {
+		outputFile = path.Base(templateName)
+		outputFile = strings.ReplaceAll(outputFile, ".tgo", ".go")
+	} else if strings.Contains(templateName, ".go.tmpl") {
+		outputFile = path.Base(templateName)
+		outputFile = strings.ReplaceAll(outputFile, ".go.tmpl", ".go")
+	}
+
 	fmt.Printf("Processing '%s'\n", xsdPath)
 	fmt.Printf("Cmd: moovio_xsd2go convert "+
 		"%s %s %s --xmlns-override=%s --template-name=%s --output-file=%s \n",
